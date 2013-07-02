5fbd43ff3dac8ec9f1e1ef79250ddd4d20ff2491
Sergey Eremeev <13.eremeev.sv@gmail.com>
2013-07-02 10:29:50 +0400

    Adding elpa folder

---
+++ elpa/zenburn-theme-1.5/zenburn-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (20945 23044
+;;;;;;  702476 472000))
+;;; Generated autoloads from zenburn-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (20945 23044 786618
+;;;;;;  685000))
+
+;;;***
+
+(provide 'zenburn-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenburn-theme-autoloads.el ends here
